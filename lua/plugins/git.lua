f2945af2dbaa486e2471cd7895272db2789321c7
Mario Alberto Chávez <mario.chavez@gmail.com>
2024-03-17 14:26:17 -0600

    Adds catpuccin and Git blame/hunk commands

---
+++ lua/plugins/git.lua
@@ -0,0 +1,7 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  keys = {
+    { "<leader>gp", ":Gitsigns preview_hunk<CR>", desc = "Git Hunk" },
+    { "<leader>gb", ":Gitsigns blame_line<CR>", desc = "Git Blame" },
+  },
+}
