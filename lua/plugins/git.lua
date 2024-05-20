55750cd78c4ae5d04683613645dc9dda069075d8
Mario Alberto Chávez <mario.chavez@gmail.com>
2024-05-20 17:39:31 -0600

    Update to latest version of Lazyvim

--- lua/plugins/git.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "lewis6991/gitsigns.nvim",
-  keys = {
-    { "<leader>gp", ":Gitsigns preview_hunk<CR>", desc = "Git Hunk" },
-    { "<leader>gb", ":Gitsigns blame_line<CR>", desc = "Git Blame" },
-  },
-}
